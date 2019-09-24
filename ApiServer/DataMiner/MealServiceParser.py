72157604b0ef64774acd6f71117941ae71f5c206
Ben Kerry <developer_kerry@naver.com>
2019-09-24 13:42:42 +0900

    Some Changes...

--- ApiServer/DataMiner/MealServiceParser.py
+++ ApiServer/DataMiner/MealServiceParser.py
@@ -2,3 +2,5 @@
 # 그것을 날짜(string):급식(list) 형태의 Key-Value를 가진 Dictionary로 변환한다.
 # 그리고 그것을 json 형태로 data/ 디렉터리에 저장한다.(확장자는 .dat)
 # 이러한 기능을 하는 코드를 run()이라는 이름의 함수로 만들기 바란다.
+
+# 새 Branch를 생성하고, 새 Branch에서 develop/DataManager를 Merge한 후 코딩을 시작하면 된다.
