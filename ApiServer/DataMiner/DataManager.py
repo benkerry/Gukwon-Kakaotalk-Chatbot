59761ba6516a4f74acef554a8b5a2f1be0069b3f
Ben Kerry <developer_kerry@naver.com>
2019-09-24 10:36:49 +0900

    Rename Variables

--- ApiServer/DataMiner/DataManager.py
+++ ApiServer/DataMiner/DataManager.py
@@ -26,31 +26,31 @@ class DataManager:
 
     def GetTimeTableData(self, week, grade, _class, week_index, time):
         # 변수 초기화!
-        file_name = {'this_week':'data/ThisWeekTimeTable.dat', 'next_week':'data/NextWeekTimeTable.dat'}
-        json_data = {'this_week':'', 'next_week':''}
+        dict_filename = {'this_week':'data/ThisWeekTimeTable.dat', 'next_week':'data/NextWeekTimeTable.dat'}
+        dict_json = {'this_week':'', 'next_week':''}
 
         # 한 번도 데이터를 파싱한 적이 없을 때 실행
-        if os.path.isfile(file_name['this_week']) == False:
-            TimeTableParser.Run(self.logger)
+        if os.path.isfile(dict_filename['this_week']) == False:
+            TimeTableParser.run(self.logger)
 
         # 저장된 JSON 파일을 읽는다.
-        with open(file_name['this_week'], 'r') as fp:
-            json_data['this_week'] = json.load(fp)
+        with open(dict_filename['this_week'], 'r') as fp:
+            dict_json['this_week'] = json.load(fp)
 
-        with open(file_name['next_week'], 'r') as fp:
-            json_data['next_week'] = json.load(fp)
+        with open(dict_filename['next_week'], 'r') as fp:
+            dict_json['next_week'] = json.load(fp)
 
         # JSON에서 (교사 번호)*100 + 과목코드로 되어있는 시간표 한 칸에 해당하는 값을 가져온다.
         try:
             if week == 0:
-                n = json_data['this_week']['자료81'][grade][_class][week_index][time]
+                n = dict_json['this_week']['자료81'][grade][_class][week_index][time]
             else:
-                n = json_data['next_week']['자료81'][grade][_class][week_index][time]
+                n = dict_json['next_week']['자료81'][grade][_class][week_index][time]
         except Exception:
             return False
 
         # 다음 주 시간표를 읽으려는데, 다음 주 시간표가 아직 안 나온 경우
-        if len(json_data['next_week']) < 50 and week = 1:
+        if len(dict_json['next_week']) < 50 and week = 1:
             return False
         
         # 수업이 없는 경우
@@ -62,6 +62,6 @@ class DataManager:
         subject_index = n % 100
 
         if week == 0:
-            return [json_data['this_week']['자료46'][teacher_index], json_data['this_week']['긴자료92'][subject_index]]
+            return [dict_json['this_week']['자료46'][teacher_index], dict_json['this_week']['긴자료92'][subject_index]]
         else:
-            return [json_data['next_week']['자료46'][teacher_index], json_data['next_week']['긴자료92'][subject_index]]
+            return [dict_json['next_week']['자료46'][teacher_index], dict_json['next_week']['긴자료92'][subject_index]]
--- ApiServer/DataMiner/TimeTableParser.py
+++ ApiServer/DataMiner/TimeTableParser.py
@@ -2,33 +2,30 @@ import requests
 import json
 import os
 
-def Run(logger):
+def run(logger):
     # 변수 초기화!!
-    file_name = {'this_week':'data/ThisWeekTimeTable.dat', 'next_week':'data/NextWeekTimeTable.dat'}
-    json_data = {'this_week':'', 'next_week':''}
-    response = {'this_week':'', 'next_week':''}
+    dict_filename = {'this_week':'data/ThisWeekTimeTable.dat', 'next_week':'data/NextWeekTimeTable.dat'}
+    dict_json = {'this_week':'', 'next_week':''}
+    dict_response = {'this_week':'', 'next_week':''}
 
     # 서버에 이번 주 시간표 데이터를 요청한다.
-    response['this_week'] = requests.get("http://112.186.146.81:4081/98372?MzQ3MzlfMzExNTRfMF8x")
-    response['this_week'].encoding = 'utf-8'
+    dict_response['this_week'] = requests.get("http://112.186.146.81:4081/98372?MzQ3MzlfMzExNTRfMF8x")
+    dict_response['this_week'].encoding = 'utf-8'
 
     # 서버에 다음 주 시간표 데이터를 요청한다.
-    response['next_week'] = requests.get("http://112.186.146.81:4081/98372?MzQ3MzlfMzExNTRfMF8y")
-    response['next_week'].encoding = 'utf-8'
+    dict_response['next_week'] = requests.get("http://112.186.146.81:4081/98372?MzQ3MzlfMzExNTRfMF8y")
+    dict_response['next_week'].encoding = 'utf-8'
 
     # 이번 주 시간표 데이터, 다음 주 시간표 데이터를 Dictionary로 변환한다.
-    json_data['this_week'] = json.loads(response['this_week'].text.split('\n')[0])
-    json_data['next_week'] = json.loads(response['next_week'].text.split('\n')[0])
+    dict_json['this_week'] = json.loads(dict_response['this_week'].text.split('\n')[0])
+    dict_json['next_week'] = json.loads(dict_response['next_week'].text.split('\n')[0])
 
     # JSON으로 파일에 저장!
-    with open(file_name['this_week'], 'w') as fp:
-        json.dump(json_data['this_week'], fp, indent='\t')
+    with open(dict_filename['this_week'], 'w') as fp:
+        json.dump(dict_json['this_week'], fp, indent='\t')
 
-    with open(file_name['next_week'], 'w') as fp:
-        json.dump(json_data['next_week'], fp, indent='\t')
+    with open(dict_filename['next_week'], 'w') as fp:
+        json.dump(dict_json['next_week'], fp, indent='\t')
     
     # 로그 남기기
     logger.Log("Data Parsing Completed.")
-
-if __name__ == "__main__":
-    Run()
