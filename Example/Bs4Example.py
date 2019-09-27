786f9725bc061cc1595be406cf910fe92d3c7391
Ben Kerry <developer_kerry@naver.com>
2019-09-27 10:13:52 +0900

    Change Exam code

--- Example/Bs4Example.py
+++ Example/Bs4Example.py
@@ -1,14 +1,17 @@
 import json
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
-response = requests.get("http://school.cbe.go.kr/gukwon-h/M01061201/list")
+str_datetime = datetime.datetime.now().strftime('%Y%m%d')
+
+response = requests.get("http://school.cbe.go.kr/gukwon-h/M01061201/list?ymd=" + str_datetime)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 dict_menu = {}
 
 for i in soup.select('li.tch-lnc-wrap dl'):
-    str_key = i.select_one('dt').text
+    str_key = i.select_one('dt').text[0]
     dict_menu[str_key] = []
     for k in i.select('li'):
         dict_menu[str_key].append(k.text)
