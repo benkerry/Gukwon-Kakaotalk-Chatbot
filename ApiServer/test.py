cacaf1c78249c8eb3ec072a075a2ea18ea0aae0f
Ben Kerry <developer_kerry@naver.com>
2019-10-04 16:04:08 +0900

    .

--- ApiServer/test.py
+++
@@ -1,6 +0,0 @@
-import ServerLogger
-import DataManager.main as DataManager
-
-logger = ServerLogger.Logger()
-manager = DataManager.Manager(logger)
-parser = DataManager.AutoParser(manager, logger)
