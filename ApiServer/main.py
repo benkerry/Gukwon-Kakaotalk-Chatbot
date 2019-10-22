6e97c90d33bbb8cc0b10b42dec9b000d0f7ea76e
Ben Kerry <developer_kerry@naver.com>
2019-10-22 11:48:41 +0900

    헬스체크, 급식 안내, 공지 테스트 관련 코드에 잘못된 요청에 대한 응답을 검증하는 코드도 추가함.

--- ApiServer/main.py
+++ ApiServer/main.py
@@ -4,46 +4,47 @@ import DataManager.main as DataManager
 
 from flask import Flask, request
 
-class ApiServer:
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    def __init__(self):
-        self.app.config['JSON_AS_ASCII'] = False
+@app.route("/")
+def healthcheck():
+    # HealthCheck 응답
+    return "Fine Working!"
 
-        self.logger = ServerLogger.Logger()
-        self.data_manager = DataManager.Manager(self.logger)
-        self.auto_parser = DataManager.AutoParser(self.data_manager, self.logger)
+@app.route("/notice-service", methods=["POST"])
+def notice_service():
+    # 공지사항 요청을 처리
+    global data_manager
+    return "공지사항 요청을 처리"
 
-    @app.route("/")
-    def healthcheck(self):
-        # HealthCheck 응답
-        return "Fine Working!"
+@app.route("/meal-notice-service", methods=["POST"])
+def meal_notice_service():
+    # 급식 알림 요청을 처리
+    global data_manager
+    return "급식 알림 요청을 처리"
 
-    @app.route("/notice-service", methods=["POST"])
-    def notice_service(self):
-        # 공지사항 요청을 처리
-        return "공지사항 요청을 처리"
+@app.route("/timetable-notice-service", methods=["POST"])
+def timetable_notice_service():
+    # 시간표 알림 요청을 처리
+    global data_manager
+    return "시간표 알림 요청을 처리"
 
-    @app.route("/meal-notice-service", methods=["POST"])
-    def meal_notice_service(self):
-        # 급식 알림 요청을 처리
-        return "급식 알림 요청을 처리"
+@app.route("/schedule-notice-service", methods=["POST"])
+def schedule_notice_service():
+    # 학사일정 알림 요청을 처리
+    global data_manager
+    return "학사일정 알림 요청을 처리"
 
-    @app.route("/timetable-notice-service", methods=["POST"])
-    def timetable_notice_service(self):
-        # 시간표 알림 요청을 처리
-        return "시간표 알림 요청을 처리"
+@app.route("/suggestion-service", methods=["POST"])
+def suggestion_service():
+    # 건의 요청을 처리(박형진 담당)
+    global data_manager
+    return "건의 요청을 처리"
 
-    @app.route("/schedule-notice-service", methods=["POST"])
-    def schedule_notice_service(self):
-        # 학사일정 알림 요청을 처리
-        return "학사일정 알림 요청을 처리"
+app.config['JSON_AS_ASCII'] = False
 
-    @app.route("/suggestion-service", methods=["POST"])
-    def suggestion_service(self):
-        # 건의 요청을 처리(박형진 담당)
-        return "건의 요청을 처리"
+logger = ServerLogger.Logger()
+data_manager = DataManager.Manager(logger)
+auto_parser = DataManager.AutoParser(data_manager, logger)
 
-if __name__ == "__main__":
-    server = ApiServer()
-    server.app.run(host="0.0.0.0", debug=False)
+app.run(host="0.0.0.0", debug=False)
--- CommTester/NoticeServiceTester.py
+++ CommTester/NoticeServiceTester.py
@@ -10,9 +10,12 @@ with open("test_data/Notice/1.json", encoding="UTF-8") as fp:
 with open("test_data/Notice/2.json", encoding="UTF-8") as fp:
     json_1 = json.load(fp)
 
-rsp_0 = requests.post("0.0.0.0:5000/notice-service", json=json_0)
-rsp_1 = requests.post("0.0.0.0:5000/notice-service", json=json_1)
+rsp_0 = requests.post("http://127.0.0.1:5000/notice-service", json=json_0)
+rsp_1 = requests.post("http://127.0.0.1:5000/notice-service", json=json_1)
+rsp_2 = requests.post("http://127.0.0.1:5000/notice-service", text="^^7")
 
 print("첫 번째 응답:\n", rsp_0.text())
 print("\n\n\n")
 print("두 번째 응답:\n", rsp_1.text())
+print("\n\n\n")
+print("잘못된 요청에 대한 응답:\n", rsp_2.text())
