b264b65e5a2bbae5ffc727f065f98de8cc7eb209
Ben Kerry <developerkerry@naver.com>
2020-08-10 14:16:06 +0900

    Last Commit

--- Webs/GukwonKakaoChatManager/functions/DeleteManager_chk.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include("session.php");
-
-    echo htmlspecialchars($_GET['nickname'])."님의 관리자 권한이 제거됩니다. 진행하시겠습니까? <a href='./DeleteManager.php?nickname=".htmlspecialchars($_GET['nickname'])."&id=".htmlspecialchars($_GET['id'])."'>진행</a>";
-?>
--- Webs/GukwonKakaoChatManager/functions/deprive.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include("session.php");
-
-    $user_val = mysqli_real_escape_string($conn, htmlspecialchars($_GET['user_val']));
-    $sql = "DELETE FROM authed_user WHERE user_val = '$user_val'";
-    mysqli_query($conn, $sql);
-
-    echo "<script>alert('유저번호: $user_val 건의 권한 박탈되었습니다.');history.back();</script>";
-?>
